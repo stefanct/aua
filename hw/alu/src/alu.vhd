18462138cdf4050f123a15754fca5cf674af7644
Stefan Rottensteiner <e0425058@student.tuwien.ac.at>
2009-01-25 14:14:27 +0000

    subc, mul* fehlen variablen tmp, tmp_reg wegbringen
    old subversion r78

--- hw/alu/src/alu.vhd
+++ hw/alu/src/alu.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 use work.aua_types.all;
 
 entity alu is
@@ -16,180 +15,223 @@ entity alu is
 end alu;
 
 architecture sat1 of alu is
-
-	signal V: bit;
 	signal C: bit;
-	constant max_value : integer := 2**word_t'length - 1;
-	constant min_value : integer := -2**(word_t'length-1); --so irgendwas halt... falls dus ueberhaupt brauchst
-
-begin
-	
-	process(opcode, opa, opb)
-		variable tmp: word_t;
+	signal V: bit;
+	--constant max_value : integer := 2**word_t'length - 1;
+	--constant min_value : integer := -2**(word_t'length-1); --so irgendwas halt... falls dus ueberhaupt brauchst
+	constant max_value: integer := 2**15-1;
+	constant min_value: integer := -2**15;
+	   
+	function calc_overflow(a: in word_t; b: in word_t) return bit is
 	begin
-		V <= '0';
-		C <= '0';
-		case opcode(5 downto 0) is
-			when "001101" => -- jmpl
-				result <= x"0000";
-			when "001110" => -- brez
-				result <= x"0000";
-			when "001111" => -- brnez
-				result <= x"0000";
+		if signed(a) > (max_value - signed(b)) and signed(b) > 0 then
+			return '1';
+		elsif signed(a) < (min_value - signed(b)) and signed(b) < 0 then
+			return '1';
+		else
+			return '0';
+		end if;
+	end function;
+	
+    begin
+		process(opcode, opa, opb)
+			variable tmp: word_t;
+			variable tmp_reg: std_logic_vector(16 downto 0);
+		begin
+			C <= '0';
+			V <= '0';
+			case opcode(5 downto 0) is
+            when "011000" => 
+				V <= calc_overflow(opa, opb);
+				result <= std_logic_vector(signed(opa) + signed(opb));
+               
+			when "011001" => 
+				V <= calc_overflow(opa, opb);
+				result <= std_logic_vector(signed(opa) + signed(opb));
+				
+			when "011010" =>
+				V <= calc_overflow(opa, opb);
+				result <= std_logic_vector(signed(opa) + signed(opb));
+				
+			when "011011" =>
+				V <= calc_overflow(opa, opb);
+				result <= std_logic_vector(signed(opa) + signed(opb));
+				
+			when "011100" => result <= x"0000";-- muli
+			when "011101" => result <= x"0000";-- muli
+			when "011110" => result <= x"0000";-- muli
+			when "011111" => result <= x"0000";-- muli
+				
 			when "100000" => -- add
-				if signed(opa) > (max_value - signed(opb)) and signed(opb) > 0 then
-					V <= '1';
-				elsif signed(opa) < (min_value - signed(opb)) and signed(opb) < 0 then
-					V <= '1';
-				else
-					V <= '0';
-				end if;
-				result <= NULL;
+				tmp_reg := std_logic_vector(('0' & unsigned(opa)) + ('0' & unsigned(opb)));
+				C <= to_bit(tmp_reg(16));
+				result <= tmp_reg(15 downto 0);
+				
 			when "100001" => -- addc
-				result <= NULL;
-			when "100010" => -- sub
-				if signed(opa) > (max_value + signed(opb)) and signed(opb) < 0 then
-					V <= '1';
-				elsif signed(opa) < (min_value + signed(opb)) and signed(opb) > 0 then
-					V <= '1';
-				else
-					V <= '0';
-				end if;result <= NULL;
+				tmp_reg := std_logic_vector(('0' & unsigned(opa)) + ('0' & unsigned(opb)));
+				if C = '1' then
+					tmp_reg := std_logic_vector(unsigned(tmp_reg) + to_unsigned(1,tmp_reg'length));
+				end if;
+				C <= to_bit(tmp_reg(16));
+				result <= tmp_reg(15 downto 0);
+				
+			when "100010" => --sub
+				tmp_reg := std_logic_vector(('0' & unsigned(opa)) - ('0' & unsigned(opb)));
+				C <= to_bit(tmp_reg(16));
+				result <= tmp_reg(15 downto 0);
+				
 			when "100011" => -- subc
-				result <= NULL;
+				tmp_reg := std_logic_vector(('0' & unsigned(opa)) - ('0' & unsigned(opb)));
+				if C = '1' then
+					tmp_reg := std_logic_vector(unsigned(tmp_reg) - to_unsigned(1,tmp_reg'length));
+				end if;
+				C <= to_bit(tmp_reg(16));
+				result <= tmp_reg(15 downto 0);
+				
 			when "100100" => -- mul
-				result <= NULL;
+				result <= x"0000";
+				
 			when "100101" => -- mulu
-				result <= NULL;
+				result <= x"0000";
+				
 			when "100110" => -- mulh
-				result <= NULL;
+				result <= x"0000";
+				
 			when "100111" => -- mulhu
-				result <= NULL;
+				result <= x"0000";
+				
 			when "101000" => -- or 
 				result <= opa or opb;
+				
 			when "101001" => -- and
 				result <= opa and opb;
+				
 			when "101010" => -- xor
 				result <= opa xor opb;
+				
 			when "101011" => -- not
-				result <= not opa;
+				result <= not opb;
+				
 			when "101100" => -- neg
---					if opa(15) = '1' then -- - to +
---						opa <= not (opa - 1);
---					else -- + to -
---						opa <= not (opa + 1);
---					end if;
-				-- ich glaub auf opa schreiben is ka gute idee!
-				-- ich glaub du willst da eher sowas wie:
-				result <= not opa(15)&opa(14 downto 0);
-				-- oda?
-			when "101101" => -- asr --?
-				-- fuer die ganzen shifts braucht ma kein tmp,
-				-- wenn man & benutzt wie ich oben beim neg
-				tmp(15) := opa(15);
-				tmp(14 downto 0) := opa(15 downto 1);
-				result <= tmp;
-			when "101110" => -- lsl --?
-				tmp(15 downto 1) := opa(14 downto 0);
-				tmp(0) := '0';
-				result <= tmp;
-			when "101111" => -- lsr --?
-				tmp(14 downto 0) := opa(15 downto 1);
-				tmp(15) := '0';
-				result <= tmp;
+				V <= '0';					
+				if opb = x"0000" then
+					result <= x"0000";
+				else
+					tmp := not opb;    
+					tmp := std_logic_vector(unsigned(tmp) + 1);
+					if opb(15) = '1' and tmp(15) = '1' then
+						V <= '1';
+					end if;
+					result <= tmp;
+				end if; 
+				
+			when "101101" => -- asr
+				result <= to_stdlogicvector(to_bitvector(opb) sra 1);
+				
+			when "101110" => -- lsl
+				result <= std_logic_vector(unsigned(opb) sll 1);
+				
+			when "101111" => -- lsr
+				result <= std_logic_vector(unsigned(opb) srl 1);
+				
 			when "110000" => -- lsli
--- geht ned
---					result <=  sll(opa, unsigned(opb(3 downto 0)));
--- auch ned... shifts broken
---					result <= std_logic_vector( unsigned(opa) sll unsigned(opb(3 downto 0)));
+				result <= std_logic_vector(unsigned(opa) sll to_integer(unsigned(opb(3 downto 0)))); 
+				
 			when "110001" => -- lsri
---					result <= opa srl unsigned(opb(3 downto 0));
+				result <= std_logic_vector(unsigned(opa) srl to_integer(unsigned(opb(3 downto 0))));
+				
 			when "110010" => -- scb
-				if opb(4) = '1' then -- set bit
-					-- auf opa schreiben is ka gute idee
---						opa(unsigned(opb(3 downto 0)) <= (others => '1'));
-					result <= opa;
-				else -- clear bit
-					-- auf opa schreiben is ka gute idee
---						opa(unsigned(opb(3 downto 0)) <= (others => '1'));
-					result <= opa;
+				tmp := x"0000";
+				tmp(to_integer(unsigned(opb(3 downto 0)))) := '1';
+				if opb(4) = '1' then -- set
+					result <= opa or tmp;
+				else -- clear
+					tmp := not tmp;
+					result <= opa and tmp;
 				end if;
+				
 			when "110011" => -- roti
---				rol broken
---					if opb(4) = '0' then -- rotl
---						result <= opa rol unsigned(opb(3 downto 0));
---					else -- rotr
---						result <= opa ror unsigned(opb(3 downto 0));
---					end if;
---				when "110100" => -- cmpv
---					result <= x"0001" when V = '1' else x"0000";
+				if opb(4) = '0' then -- rotl
+					result <= std_logic_vector(unsigned(opa) rol to_integer(unsigned(opb(3 downto 0))));
+				else -- rotr
+					result <= std_logic_vector(unsigned(opa) ror to_integer(unsigned(opb(3 downto 0))));
+				end if;
+            
+			when "110100" => -- cmpv
+				if V = '1' then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;  
+				
 			when "110101" => -- cmplt
---					result <= x"0001" when signed(opa) < signed(obb) else x"0000";
+				if signed(opa) < signed(opb) then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;
+				
 			when "110110" => -- cmpltu
---					result <= x"0001" when opa < opb else x"0000";
+				if opa < opb then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;
+				
 			when "110111" => -- cmplte
---					result <= x"0001" when signed(opa) <= signed(obb) else x"0000";
+				if signed(opa) <= signed(opb) then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;
+				
 			when "111000" => -- cmplteu
-				-- warum compiled das when scheissdrex ned da ueberall?
---					result <= x"0001" when (opa <= opb) else x"0000";
 				if opa <= opb then
 					result <= x"0001";
 				else
 					result <= x"0000";
 				end if;
---				when "111001" => -- cmpe
---					result <= x"0001" when opa = opb else x"0000";
---				when "111010" => -- cmpei
---					tmp(15 downto 5) := (others => '0');
---					tmp(4 downto 0) := opb(4 downto 0);
---					result <= x"0001" when unsigned(opa) = unsigned(tmp) else x"0000";
-			when "111011" => -- mov
-				result <= opb;
-			when "111100" => -- ld
-				result <= x"0000";
-			when "111101" => -- ldb
-				result <= x"0000";
-			when "111110" => -- st
-				result <= x"0000";
-			when "111111" => -- stb
-				result <= x"0000";
-			when "000000" => -- ldi
-			when "000001" => -- ldi
-			when "000010" => -- ldi
-			when "000011" => -- ldi
-			when "000100" => -- ldi
-			when "000101" => -- ldi
-			when "000110" => -- ldi
-			when "000111" => -- ldi
-				result(15 downto 8) <= (others => '0');
-				result(7 downto 0) <= opb(7 downto 0);
-			when "010000" => -- brezi
-			when "010001" => -- brezi
-			when "010010" => -- brezi
-			when "010011" => -- brezi
-				result <= x"0000";
-			when "010100" => -- brnezi
-			when "010101" => -- brnezi
-			when "010110" => -- brnezi
-			when "010111" => -- brnezi 
-				result <= x"0000";
-			when "011000" => -- addi
-			when "011001" => -- addi
-			when "011010" => -- addi
-			when "011011" => -- addi
-				result <= NULL;
-			when "011100" => -- muli
-			when "011101" => -- muli
-			when "011110" => -- muli
-			when "011111" => -- muli
-				result <= NULL;
-			when others =>
-				result <= x"0000";
-		end case;
-	end process;
-end sat1;
-
---todo:
---muli
---addi
---
+				
+			when "111001" => -- cmpe
+				if opa = opb then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;
+				
+			when "111010" => -- cmpei
+			-- wie gehtn das richtig				    
+				if opa = (("00000000000") & opb(4 downto 0)) then
+					result <= x"0001";
+				else
+					result <= x"0000";
+				end if;
+					
+			when "000000" => result <= x"0000";-- ldi
+				when "000001" => result <= x"0000";-- ldi
+				when "000010" => result <= x"0000";-- ldi
+				when "000011" => result <= x"0000";-- ldi
+				when "000100" => result <= x"0000";-- ldi
+				when "000101" => result <= x"0000";-- ldi
+				when "000110" => result <= x"0000";-- ldi
+				when "000111" => result <= x"0000";-- ldi
+            when "001101" => result <= x"0000";-- jmpl
+			when "001110" => result <= x"0000";-- brez
+			when "001111" => result <= x"0000";-- brnez
+			when "010000" => result <= x"0000";-- brezi
+				when "010001" => result <= x"0000";-- brezi
+				when "010010" => result <= x"0000";-- brezi
+				when "010011" => result <= x"0000";-- brezi
+			when "010100" => result <= x"0000";-- brnezi
+				when "010101" => result <= x"0000";-- brnezi
+				when "010110" => result <= x"0000";-- brnezi
+				when "010111" => result <= x"0000";-- brnezi 
+            when "111011" => result <= opb;-- mov
+				when "111100" => result <= x"0000";-- ld
+				when "111101" => result <= x"0000";-- ldb
+				when "111110" => result <= x"0000";-- st
+				when "111111" => result <= x"0000";-- stb
+				when others => result <= x"0000";
+			end case;
+		end process;
+    end sat1;
