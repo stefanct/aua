f248c898b73fa9ad410595618f28d9b0276c302f
Jakob Wilhelm <jakob.wilhelm@student.tuwien.ac.at>
2009-01-26 22:13:05 +0000

    Testprogramm digit.asm Update
    old subversion r104

--- as/test/digit.asm
+++ as/test/digit.asm
@@ -1,24 +1,39 @@
 -- Simpcon Adresse für Digits: 0xff10
 -- ! markiert Soll-Ergebnisse zum Vergleichen
-ldi $3, 0xff ! 0x1fe1
-lsli $3, 8 ! 0xc101
-addi $3, 0x10 ! 0x6201
+addi $3, 0x3f
+nop
+lsli $3, 2
+nop
+addi $3, 3
+lsli $3, 6
+addi $3, 0x10
 
-ldi $4, 0x66 -- 7-Segment 4: 01100110
+addi $4, 0x6 -- 7-Segment 4: 01100110
+lsli $4, 4
+addi $4, 0x6
 stb $3, $4
 
-ldi $4, 0x4f -- 3: 01001111
+addi $4, 0x4 -- 3: 01001111
+lsli $4, 4
+addi $4, 0xf
 addi $3, 1 -- nächstes Digit adressieren
 stb $3, $4
 
-ldi $4, 0x5b -- 2: 01011011
+addi $4, 0x5 -- 2: 01011011
+lsli $4, 4
+addi $4, 0xb
 addi $3, 1
 stb $3, $4
 
-ldi $4, 0x06 -- 1:  00000110
+addi $4, 0x06 -- 1:  00000110
 addi $3, 1
 xxx:
 stb $3, $4
+sb $4, 1
+sb $2, 3
+cb $6, 2
+cb $1, 7
+nop
 
 loop:
 	rjmpi loop -- damit wir nicht _irgendwas_ machen
