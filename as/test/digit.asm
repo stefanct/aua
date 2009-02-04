7936521558a5d61435642db7288df8053ebf8494
Jakob Wilhelm <jakob.wilhelm@student.tuwien.ac.at>
2009-02-04 08:48:08 +0000

    digit neue Version
    old subversion r129

--- as/test/digit.asm
+++ as/test/digit.asm
@@ -1,15 +1,38 @@
--- Simpcon Adresse für Digits: 0xff10
--- ! markiert Soll-Ergebnisse zum Vergleichen
-
 #include libaua/io/digit.h
 
 ldiw $1, SC_DIGITS
 
 ldiw $2, SEGS
+mov $6, $2
 
-ld $3, $2
+ldi $4, 10 -- Top value
 
-st $3, $1
+count_0:
+	ldi $3, 0 -- Counter
+
+count:
+	mov $5, $4 -- counter schon bei top? Dann wieder auf 0 setzen
+	sub $5, $3
+	brezi $5, count_0
+
+	nop
+	nop
+	mov $6, $2 -- Digit aus Array suchen
+	add $6, $3
+	nop
+	nop
+
+	st $3, $1 -- Wert auf Digit schreiben
+
+	addi $3, 1 -- Counter erhöhen
+
+rjmpi count
+
+
+ldiw $10, 0xffff
+wait:
+	addi $10, -1
+	brezi $10, wait
 
 loop:
 	rjmpi loop -- damit wir nicht _irgendwas_ machen
