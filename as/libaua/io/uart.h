d11a15c06d4d99145af301e2e0998e2fd5451288
Jakob Wilhelm <jakob.wilhelm@student.tuwien.ac.at>
2009-02-12 22:32:04 +0000

    Bootloader begonnen, Funktion uart_read in uart.h
    old subversion r177

--- as/libaua/io/uart.h
+++ as/libaua/io/uart.h
@@ -1,1 +1,36 @@
 #define SC_UART 0xff20
+#define SC_UART_STATUS 0xff20
+#define SC_UART_DATA 0xff21
+
+-- Params: address, length (in word)
+-- Returns: void (undefined)
+uart_read:
+	push $1
+	push $2
+	push $3
+	push $4
+	push $10 -- Adresse, wohin das Zeug soll
+	push $11 -- Anzahl an words zu lesen
+
+	ldiw $1, SC_UART_STATUS
+	ldiw $4, SC_UART_DATA
+  loop_read:
+	ldi $3, 1 -- Konstante 1
+	ld $2, $1 -- UART Status in $2
+	and $3, $2 -- Bit 0 in Status sagt, ob was zu lesen da
+	brezi $3, loop_read -- nichts zu lesen => warten
+
+	ld $2, $4 -- Daten lesen
+	st $2, $10 -- Daten speichern
+	addi $10, 2 -- Adresse zum Speicher +2
+	addi $11, -1 -- wieder ein word weniger zu lesen
+
+	brnezi $11, loop_read -- solang wir noch lesen müssen, tun wir das brav
+
+	pop $1
+	pop $2
+	pop $3
+	pop $4
+	pop $10
+	pop $11
+	ret
---
+++ as/test/boot.asm
@@ -0,0 +1,21 @@
+#include libaua/stdlib.h
+#include libaua/io/uart.h
+#include libaua/io/digit.h
+
+ldiw $fp, STACK_TOP
+ldiw $sp, STACK_TOP
+
+ldiw $16, SC_DIGITS
+
+-- Größe von Image in ersten beiden Bytes von UART
+ldiw $10, 0 -- UART Zielpointer für Daten
+ldiw $11, 1 -- Anzahl words zu lesen
+
+ldiw $2, uart_read -- Adresse von Funktion uart_read
+
+call $2
+
+ld $11, $10 -- jetzt lesen wir soviel, wie das Image lang ist
+st $11, $16
+
+call $2
